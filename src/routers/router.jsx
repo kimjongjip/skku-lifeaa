d03f43dcbe58b916285482fcfdb28b06f1b7785d
chrimp <60501603+chrimp@users.noreply.github.com>
2024-11-04 17:01:01 +0900

    html redirection

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,17 +1,18 @@
 // Router: 각 URL에 따른 page 컴포넌트 연결
 import { createBrowserRouter } from "react-router-dom";
+import HtmlLoader from "../components/common/HtmlLoader";
 
 import LoginPage from "../pages/LoginPage";
 import SignupPage from "../pages/SignupPage";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LoginPage />,
-    idnex: true,
+    element: <HtmlLoader file="/html/templates/login.html" />,
+    index: true,
   },
   {
     path: "/signup",
-    element: <SignupPage />,
+    element: <HtmlLoader file="/html/templates/signup.html" />,
     index: true,
   },
 ]);

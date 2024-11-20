3391a97733ae568b7cca4a765054c5c112b99c8c
kimjongjip <yranj86@gmail.com>
2024-11-20 13:52:30 +0900

    통합

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -1,10 +1,19 @@
 import React, {useState} from "react";
 import {Tabs, Tab, Box} from '@mui/material';
 import {TabContext,TabList,TabPanel} from '@mui/lab';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Nav() {
-    const [value, setValue] = useState("1"); 
+    const location = useLocation();
+    const [value, setValue] = useState(() => {
+        switch(location.pathname) {
+            case '/main': return "1";
+            case '/certificate': return "2";
+            case '/penalty': return "3";
+            case '/management': return "4";
+            default: return "1";
+        }
+    });
 
   const handleChange = (event, newValue) => {
     setValue(newValue); 
@@ -24,10 +33,10 @@ export default function Nav() {
 <TabContext value={value} sx={{height: "40px"}} >
   <Box sx={{ borderBottom: 1, borderColor: 'divider', height: "40px", width: "100%"}}>
   <TabList onChange={handleChange} sx={{minHeight: "40px", height: "40px", width: "100%"}} centered>
-  <Tab component={Link} to="/" label="메인" value="1" sx={{padding: "0", minHeight: "40px", height: "40px"}} />
-  <Tab component={Link} to="/" label="인증" value="2" sx={{padding: "0", minHeight: "40px", height: "40px"}} />
-  <Tab component={Link} to="/" label="벌칙" value="3" sx={{padding: "0", minHeight: "40px", height: "40px"}} />
-  <Tab component={Link} to="/" label="모임관리" value="4" sx={{padding: "0", minHeight: "40px", height: "40px"}}/>
+  <Tab component={Link} to="/main" label="메인" value="1" sx={{padding: "0", minHeight: "40px", height: "40px"}} />
+  <Tab component={Link} to="/certificate" label="인증" value="2" sx={{padding: "0", minHeight: "40px", height: "40px"}} />
+  <Tab component={Link} to="/penalty" label="벌칙" value="3" sx={{padding: "0", minHeight: "40px", height: "40px"}} />
+  <Tab component={Link} to="/management" label="모임관리" value="4" sx={{padding: "0", minHeight: "40px", height: "40px"}}/>
 </TabList>
   </Box>
 </TabContext>

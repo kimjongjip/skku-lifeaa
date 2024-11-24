c1a21feffdd825622ec3c159cf1b6ab4795a0940
kimjongjip <yranj86@gmail.com>
2024-11-24 12:43:24 +0900

    git action 연동 추가

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -15,5 +13,10 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts', // setup.ts 파일 경로
     include: ['tests/**/*.test.jsx', 'tests/**/*.test.tsx'],
+    coverage: {
+      provider: 'c8', // c8을 사용해 커버리지 리포트를 생성
+      reporter: ['text', 'lcov', 'clover'], // lcov, clover 리포터를 추가
+      reportsDirectory: './coverage', // 커버리지 리포트 디렉토리 설정
+    },
   },
 });
